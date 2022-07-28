cffaae32041461588d7e4ccbb331d8537ad54bbb
tMirdin <mirdin.981@gmail.com>
2022-07-28 17:49:56 +0600

    Финиш

--- src/components/AddContact/AddContact.js
+++ src/components/AddContact/AddContact.js
@@ -1,44 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const AddContact = (props) => {
-let [name, setName] = useState('');
-let [lastName, setLastName] = useState('');
-let [photo, setPhoto] = useState('');
+  let [name, setName] = useState("");
+  let [lastName, setLastName] = useState("");
+  let [photo, setPhoto] = useState("");
 
-function handleClick(){
+  function handleClick() {
     let newContact = {
-        name,
-        lastName,
-        photo,
-        id: Date.now()
-    }
-    props.handleNewContact(newContact)
-    setName('')
-    setLastName('')
-    setPhoto('')
-}
+      name,
+      lastName,
+      photo,
+      id: Date.now(),
+    };
+    props.handleNewContact(newContact);
+    setName("");
+    setLastName("");
+    setPhoto("");
+  }
 
-    return (
-        <div>
-            <input 
-                onChange={(e)=> setName(e.target.value)} 
-                type="text" placeholder="Name"
-                value={name}
-                />
-            <input 
-                onChange={(e)=> setLastName(e.target.value)} 
-                type="text" placeholder="LastName"
-                value={lastName}
-                />
-            <input 
-                onChange={(e)=> setPhoto(e.target.value)} 
-                type="url" placeholder="URL Photo"
-                value={photo}
-                />
-            <button onClick={handleClick}>Add contact</button>
-        </div>
-
-    );
+  return (
+    <div>
+      <input
+        onChange={(e) => setName(e.target.value)}
+        type="text"
+        placeholder="Name"
+        value={name}
+      />
+      <input
+        onChange={(e) => setLastName(e.target.value)}
+        type="text"
+        placeholder="LastName"
+        value={lastName}
+      />
+      <input
+        onChange={(e) => setPhoto(e.target.value)}
+        type="url"
+        placeholder="URL Photo"
+        value={photo}
+      />
+      <button onClick={handleClick}>Add contact</button>
+    </div>
+  );
 };
 
 export default AddContact;
